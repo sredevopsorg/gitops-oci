0375a944857ece56b1197b79eac979f2236c12e2
Nicolás Georger <info@sredevops.dev>
2023-02-08 05:59:40 -0300

    prerelease

--- clusters/oci/variables.tf
+++ clusters/oci/variables.tf
@@ -6,11 +6,11 @@ variable "ssh_authorized_keys" {}
 variable "region" {}
 variable "tenancy_ocid" {}
 
-variable "ssh_public_key" {} 
+/* variable "ssh_public_key" {} 
 variable "image_ocid" {}
 variable "ssh_private_key_path" {}
 variable "ssh_private_key" {}
 variable "cluster_token" {}
 variable "compartment_ocid" {}
 variable "current_user_ocid" {}
-variable "private_key" {}
+variable "private_key" {} */
