9c21ef1ab874fa1dcc235ba8c58e9b6045d3cba3
Nicolás Georger <info@sredevops.dev>
2023-02-10 21:18:43 -0300

    prerelease, no shared vol

--- clusters/oci/storage.tf
+++ clusters/oci/storage.tf
@@ -1,4 +1,4 @@
-resource "oci_core_volume" "shared_50gb_oci_core_volume" {
+/* resource "oci_core_volume" "shared_50gb_oci_core_volume" {
 	availability_domain = "CQhm:SA-SANTIAGO-1-AD-1" # Availability Domain for Chile, if you are in another zone you should review this 
 	compartment_id = var.compartment_id
 	display_name = "50gb_shared"
@@ -8,4 +8,4 @@ resource "oci_core_volume" "shared_50gb_oci_core_volume" {
 	is_auto_tune_enabled = "false"
 	size_in_gbs = "50"
 	vpus_per_gb = "10"
-}
+} */
